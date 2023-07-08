b67f20ebe9c5b57e7a049a65391010d71c1185c6
underlinear <arduouschore@gmail.com>
2023-07-09 00:12:05 +0800

    Completed exercises 5-7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(number1,number2) {
+    let start,end
+    let result = 0
+    if ((number1 < 0 || number2 < 0) || typeof number1 != "number" || typeof number2 != "number")
+    {
+        return "ERROR"
+    }
+    if(number1 > number2)
+    {
+        start=number2
+        end=number1
+    }
+    else
+    {
+        start=number1
+        end=number2
+    }
+    for(let i=start;i<=end;i++)
+    {
+        result += i
+    }
+    return result
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let floatCelsius = (fahrenheit -32) * (5/9)
+  let result = Math.round(floatCelsius*10)/10
+  return result
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let floatFahrenheit = (celsius*(9/5)) + 32
+  let result = Math.round(floatFahrenheit*10)/10
+  return result
 };
 
 // Do not edit below this line
