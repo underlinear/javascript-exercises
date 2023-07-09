8a9b6675780bf4a865311106dd34b7b402bac495
underlinear <arduouschore@gmail.com>
2023-07-10 02:38:55 +0800

    Finished 8-9 tasks

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,55 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+//DON'T USE REST PARAMETER ON A ARRAY ARGUMENT
+const sum = function(addend) {
+	/*
+  let sum = 0;
+	for(let i=0;i<addend.length;i++)
+  {
+    sum += addend[i];
+  }
+  return sum;
+  */
+  /*
+  let sum = 0;
+  for(let addends of addend)
+  {
+  sum+=addends;
+  }
+  return sum;
+  */
+  
+  return addend.reduce((a,b)=>a+b,0);
+  
+
 };
 
-const multiply = function() {
-
+const multiply = function(...factor) {
+  let product = 1;
+	for(let i=0;i<factor.length;i++)
+  {
+    product *= factor[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(num,power) {
+	return num ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let factorial = 1;
+	for(let i=2;i<=num;i++)
+  {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,28 @@
-const palindromes = function () {
+const palindromes = function (stringArg) {
+    let newstring = '';
+    let string = stringArg.replace(/[^a-zA-Z0-9]/g, '')
 
+    for(let i=0;i<string.length;i++)
+    {
+        newstring += string[i];
+    }
+    
+    newstring = newstring.split('');
+    newstring.reverse();
+    let newarray = newstring.join('');
+    let reversedstring = newarray.toLowerCase();
+    let normalstring = string.toLowerCase();
+    console.log(reversedstring);
+    console.log(normalstring);
+    if(reversedstring.trim()===normalstring)
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
